27b8ca898797c38aca83b0e1c868ad9769dd4aa1
AloneDc <danieleduar0911@gmail.com>
2025-04-28 16:59:29 -0500

    fix: modificar login y server

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,7 @@ const estatuasRoutes = require("./routes/estatuas");
 const app = express();
 
 // Middlewares
-app.use(
-  cors({
-    origin: "https://proyecto-estatuas.vercel.app", // tu frontend de producción
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "public")));
 
